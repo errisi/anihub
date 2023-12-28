d79be14d69f23cb8e8aa5c4ae2a3b7bf3cc9e66f
Dmytro Krasnoholovenko <errisi.v@gmail.com>
2023-12-28 22:55:10 +0200

    clear

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+const App = () => (
+  <>
+    <div>
+      <a href="https://react.dev" target="_blank" rel="noreferrer">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+    </div>
+  </>
+);
+
+export default App;
