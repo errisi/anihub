dd2e49a456f7ea97f797092777f2ccb2060c39f7
Dmytro Krasnoholovenko <errisi.v@gmail.com>
2024-02-23 23:56:29 +0200

    added simple player

--- src/components/Anime/AnimePlayer/AnimePlayer.tsx
+++ src/components/Anime/AnimePlayer/AnimePlayer.tsx
@@ -1,5 +1,5 @@
-import { Box } from '@mui/material';
-import { FC } from 'react';
+import axios from 'axios';
+import { FC, useEffect, useState } from 'react';
 import styles from './AnimePlayer.module.scss';
 import { AnimeDescription } from '../../../types/AnimeDescription';
 
@@ -7,18 +7,29 @@ type Props = {
   anime: AnimeDescription;
 };
 
-export const AnimePlayer: FC<Props> = ({ anime }) => (
-  <div className={styles.anime__player}>
-    <h2 className={styles.anime__player__title}>
-      {`Смотреть аниме «${anime.russian}» онлайн`}
-    </h2>
-    <Box
-      sx={{
-        width: '100%',
-        height: 586,
-        bgcolor: 'white',
-        marginBottom: '26px',
-      }}
-    />
-  </div>
-);
+export const AnimePlayer: FC<Props> = ({ anime }) => {
+  const [playerLink, setPlayerLink] = useState('');
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://kodikapi.com/search?token=d884e7fbe01111ee8c84d78cdc023249&shikimori_id=${anime.id}`,
+      )
+      .then((link) => setPlayerLink(link.data.results[0].link));
+  }, [anime]);
+
+  return (
+    <div className={styles.anime__player}>
+      <h2 className={styles.anime__player__title}>
+        {`Смотреть аниме «${anime.russian}» онлайн`}
+      </h2>
+      <iframe
+        title="qwe"
+        src={playerLink}
+        frameBorder="0"
+        allow="autoplay *; fullscreen *"
+        className={styles.anime__player__content}
+      />
+    </div>
+  );
+};
